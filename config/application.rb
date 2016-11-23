d47c19b41dab350708797d2ac31b0c95fe8f676f
Christine Oo <ootjinvoon@gmail.com>
2016-11-23 16:49:40 +0800

    Add cors

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,13 @@ Bundler.require(*Rails.groups)
 
 module QuizMaster
   class Application < Rails::Application
+    config.middleware.insert_before 0, "Rack::Cors" do
+          allow do
+            origins 'localhost:8080'
+            resource '*', :headers => :any, :methods => [:get, :post, :put, :options, :delete]
+          end
+        end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
