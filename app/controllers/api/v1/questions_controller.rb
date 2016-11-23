1fa8b0c3eb2a32775ea8312677f7a42179ad3767
Christine Oo <ootjinvoon@gmail.com>
2016-11-23 16:51:30 +0800

    Add create, update delete and quiz mode api

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,7 +1,43 @@
 class Api::V1::QuestionsController < ApplicationController
+    skip_before_action :verify_authenticity_token
 
     def index
         render json: Question.all
+    end
+
+    def create
+        render json: Question.create(question_params)
+    end
+
+    def update
+        question = Question.find(params[:id])
+        question.update(question_params)
+        render json: {status: "ok", message: "Question with id #{params[:id]} has been updated."}
+    end
+
+    def destroy
+        question = Question.find(params[:id])
+        question.destroy
+		render json: {status: "ok", message: "Question with id #{params[:id]} has been deleted."}
+    end
+
+    def start_quiz
+        render json: {id: Question.first.id, content: Question.first.content}
+    end
+
+    def check_answer
+        answer = Question.find(params[:id]).answer
+        if(answer == params[:inputAnswer])
+            render json: {status: "ok"}
+        else
+            render json: {status: "error", expected: answer}
+        end
+    end
+
+    private
+
+    def question_params
+      params.require(:question).permit!
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
       resources :questions
+      get '/start_quiz' => 'questions#start_quiz'
+      get '/next_question/:id' => 'questions#next_question'
+      post '/check_answer/:id' => 'questions#check_answer'
     end
   end
   # The priority is based upon order of creation: first created -> highest priority.
