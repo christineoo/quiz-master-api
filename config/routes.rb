e0f8f9aa633d243b5c54185343a0f4fcc137f98d
Christine Oo <ootjinvoon@gmail.com>
2016-12-04 19:11:39 +0900

    Clean up unused routes and specify routes for questions instead of using resources as resources creates a lot of unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
-      resources :questions
+      get '/questions' => 'questions#index'
+      get '/questions/:id' => 'questions#show'
+      post '/questions' => 'questions#create'
+      put '/questions/:id' => 'questions#update'
+      delete 'questions/:id' => 'questions#destroy'
       get '/start_quiz' => 'questions#start_quiz'
-      get '/next_question/:id' => 'questions#next_question'
       post '/check_answer/:id' => 'questions#check_answer'
     end
   end
